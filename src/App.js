8c0a843e9292277e0b1e1ac0b29c6b38a91d6be3
firstquarterlee <firstquarterlee@gmail.com>
2023-08-15 15:58:44 +0900

    concat, filter, map, slice, 스프레드연산자

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Wood from "./wood";
+
+// 0. React 엔진 - 데이터변경감지해서 UI그려주는~!
+// 1. 실행과정 (index.html) - 싱글페이지어플리케이션(SPA)
+// 2. JSX 문법
 
 function App() {
+  let list = [1, 2, 3];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {list.map((n) => (
+          <h1>{n}</h1>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/wood.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Wood() {
+  return (
+    <div>
+      <div>나무</div>
+    </div>
+  );
+}
+
+export default Wood;
