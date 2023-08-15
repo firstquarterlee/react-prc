90b8728334874579a295979cda0cfae2becb42ed
firstquarterlee <firstquarterlee@gmail.com>
2023-08-16 01:43:08 +0900

    useState 문제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
-import Sub from "./Sub";
 
-// let number = 1; // 상태 값 아님
+const App = () => {
+  // 다운로드 받음
+  const [users, setUsers] = useState([]);
 
-export default function App() {
-  const [number, setNumber] = useState(2); // React안에 hooks 라이브러리가 있다. 그래서 쓸 수 있음. 상태값이 됨.
-  // 랜더링 시점 = 상태값 변경
+  const download = () => {
+    let sample = [
+      { id: 1, name: "홍길동" },
+      { id: 2, name: "임꺽정" },
+      { id: 3, name: "장보고" },
+      { id: 4, name: "이순신" },
+    ];
 
-  const add = () => {
-    setNumber(number + 1); // 리액트한테 number 값 변경할께! 라고 요청하는것이다.
-    console.log("add", number);
+    setUsers(sample);
   };
 
   return (
     <div>
-      <h1>숫자:{number}</h1>
-      <button onClick={add}>더하기</button>
-      <Sub />
+      <button onClick={download}>다운로드</button>
+      {users.map((u) => (
+        <h1>
+          {u.id},{u.name}
+        </h1>
+      ))}
     </div>
   );
-}
+};
 
-// add() 이건 바로 실행한다는거다! 쓰지마셈.
+export default App;
